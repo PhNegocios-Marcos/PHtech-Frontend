20ee707adaf087eac78aa27183144ddd7fa74381
zecawuw <joaopedrosantosdonascimentodev@gmail.com>
2025-07-16 12:02:10 -0300

    ajuste na tela de login

--- app/dashboard/(guest)/login/page.tsx
+++ app/dashboard/(guest)/login/page.tsx
@@ -4,19 +4,14 @@ import Link from "next/link";
 import { useState } from "react";
 import { z } from "zod";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle
-} from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import FA from "./components/2FA";
 import Promotoras from "./components/promotoras";
 import { useAuth } from "@/contexts/AuthContext";
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+import Image from "next/image";
 
 type ModalType = "none" | "2FA" | "modal2" | "promotoras";
 
@@ -33,13 +28,7 @@ export default function Page() {
   const [errors, setErrors] = useState<{ email?: string; password?: string }>({});
   const [loginError, setLoginError] = useState<string | null>(null);
 
-  const {
-    setMail,
-    setSenha,
-    setPromotoras,
-    setId,
-    id
-  } = useAuth();
+  const { setMail, setSenha, setPromotoras, setId, id } = useAuth();
 
   const handleLogin = async () => {
     const result = loginSchema.safeParse({ email, password });
@@ -101,9 +90,16 @@ export default function Page() {
   const closeModalPromotoras = () => setPromotorasModal("none");
 
   return (
-    <div className="flex items-center justify-center py-4 lg:h-screen">
+    <div className="flex flex-col bg-gradient-to-tr from-[#c91212] to-[#6d2516] items-center justify-center py-4 lg:h-screen">
       {currentModal === "none" && promotorasModal === "none" && (
         <Card className="mx-auto w-96">
+          <Image
+            src="/logo.png" // caminho da imagem (pública ou importada)
+            alt="Descrição da imagem"
+            width={200}
+            height={200}
+            className="mx-auto"
+          />
           <CardHeader>
             <CardTitle className="text-2xl">Login</CardTitle>
             <CardDescription className="text-[13px]">
@@ -122,17 +118,14 @@ export default function Page() {
                   onChange={(e) => setEmail(e.target.value)}
                   required
                 />
-                {errors.email && (
-                  <p className="text-sm text-red-500">{errors.email}</p>
-                )}
+                {errors.email && <p className="text-sm text-red-500">{errors.email}</p>}
               </div>
               <div className="grid gap-2">
                 <div className="flex items-center">
                   <Label htmlFor="password">Senha</Label>
                   <Link
                     href="/dashboard/forgot-password"
-                    className="ml-auto inline-block text-sm underline"
-                  >
+                    className="ml-auto inline-block text-sm underline">
                     Esqueceu sua senha?
                   </Link>
                 </div>
@@ -143,13 +136,9 @@ export default function Page() {
                   onChange={(e) => setPassword(e.target.value)}
                   required
                 />
-                {errors.password && (
-                  <p className="text-sm text-red-500">{errors.password}</p>
-                )}
+                {errors.password && <p className="text-sm text-red-500">{errors.password}</p>}
               </div>
-              {loginError && (
-                <p className="text-center text-sm text-red-500">{loginError}</p>
-              )}
+              {loginError && <p className="text-center text-sm text-red-500">{loginError}</p>}
               <Button onClick={handleLogin} type="button" className="w-full">
                 Login
               </Button>
@@ -161,10 +150,7 @@ export default function Page() {
       {currentModal === "2FA" ? (
         <FA onNext={() => setCurrentModal("modal2")} onClose={closeModal} />
       ) : promotorasModal === "promotoras" ? (
-        <Promotoras
-          onNext={() => setCurrentModal("modal2")}
-          onClose={closeModalPromotoras}
-        />
+        <Promotoras onNext={() => setCurrentModal("modal2")} onClose={closeModalPromotoras} />
       ) : null}
     </div>
   );
