e198b9472c7d94ad0ef08c490fe6ebd68db9b6ce
zecawuw <joaopedrosantosdonascimentodev@gmail.com>
2025-07-29 14:03:15 -0300

    90%

--- app/dashboard/(auth)/cadastro/produto/page.tsx
+++ app/dashboard/(auth)/cadastro/produto/page.tsx
@@ -8,26 +8,8 @@ import { useHasPermission } from "@/hooks/useFilteredPageRoutes";
 import CadastroTabelaModal from "./components/cadastroTabela";
 import CampoBoasVindas from "@/components/boasvindas";
 import Produto from "./components/produtos";
+import { Props } from "./components/produtos";
 
-export type Produto = {
-  id?: string;
-  nome?: string;
-  status?: number;
-  idade_minima?: number;
-  idade_maxima?: number;
-  prazo_minimo?: number;
-  prazo_maximo?: number;
-  id_uy3?: string | null;
-  cor_grafico?: string | null;
-  config_tabela_hash?: string;
-  usuario_atualizacao?: string;
-  tabela_hash?: string;
-};
-
-type Props = {
-  produto: Produto;
-  onClose: () => void;
-};
 
 
 export default function Page({produto}: Props) {
@@ -40,12 +22,15 @@ export default function Page({produto}: Props) {
     <ProtectedRoute requiredPermission="Subprodutos_ver">
       <div className="mb-4 flex justify-between space-y-4">
         <CampoBoasVindas />
-        <Button onClick={() => setIsCadastroOpen(true)}>Novo Produto</Button>
+
+        <Button id="" onClick={() => setIsCadastroOpen(true)}>
+          Novo Produto
+        </Button>
       </div>
 
       {!isCadastroOpen && <TaxaProduto produto={produto} onClose={handleCloseCadastro} />}
+
       <CadastroTabelaModal isOpen={isCadastroOpen} onClose={handleCloseCadastro} />
     </ProtectedRoute>
   );
 }
-
